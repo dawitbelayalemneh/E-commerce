42b7385d5390f82b4e9a6cb36ece9b075f4bf341
dawitBelayA <dawitbelay@amplitude.ventures>
2026-07-29 01:47:49 +0300

    created serializers for category and products

--- backend/store/views.py
+++ backend/store/views.py
@@ -1,8 +1,16 @@
-from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
 
-# Create your views here.
-def home(request):
-    data = {
-        'message': 'Hello, World!'
-    }
-    return JsonResponse(data)
+@api_view(['GET'])
+def get_products(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data) 
+
+@api_view(['GET'])
+def get_categories(request):
+    categories = Category.objects.all()
+    serializer = CategorySerializer(categories, many=True)
+    return Response(serializer.data)
