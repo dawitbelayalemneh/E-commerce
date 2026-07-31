fbcb36ee99e355691aa6346b4f09130cb74b47b8
dawitBelayA <dawitbelay@amplitude.ventures>
2026-07-31 22:50:46 +0300

    added the backend logic for cart functionality

--- backend/store/urls.py
+++ backend/store/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('products/', views.get_products),
     path('products/<int:pk>/', views.get_product),
     path('categories/', views.get_categories),
+    path('cart/', views.get_cart),
+    path('cart/add/', views.add_to_cart),
+    path('cart/remove/<int:item_id>/', views.remove_from_cart),
 ]
